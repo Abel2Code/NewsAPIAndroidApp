4c9df2c7094e811978538d098c7a766c80673b27
Abel2Code <asalinaswc1@aol.com>
2018-10-23 12:32:38 -0700

    Initialized Project

---
+++ app/src/main/java/com/example/abel/newsapiandroidapp/utilities/NetworkUtils.java
@@ -0,0 +1,9 @@
+package com.example.abel.newsapiandroidapp.utilities;
+
+public class NetworkUtils {
+    final static String NEWS_BASE_URL = "https://newsapi.org/v1/articles";
+    final static String NEWS_QUERY_SOURCE="the-next-web";
+    final static String NEWS_QUERY_SORTBY = "latest";
+    final static String NEWS_QUERY_APIKEY = SecretKeys.NEWS_API_KEY;
+
+}
