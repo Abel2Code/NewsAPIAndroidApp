21ea26d15840e307a0c044ec2c6d62f339a59d4e
Abel2Code <asalinaswc1@aol.com>
2018-10-24 11:54:25 -0700

    Added Search and Search Refresh Button

--- app/src/main/java/com/example/abel/newsapiandroidapp/MainActivity.java
+++ app/src/main/java/com/example/abel/newsapiandroidapp/MainActivity.java
@@ -1,13 +1,134 @@
 package com.example.abel.newsapiandroidapp;
 
+import android.net.Network;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.abel.newsapiandroidapp.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String>{
+    private static final String TAG = "MainActivity";
+    private String newsSearchResults;
+    private static final int LOADER_ID = 1;
+
+    private Toolbar mToolbar;
+    private ProgressBar mProgressBar;
+    private TextView mTextView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onCreateLoader(0 , savedInstanceState);
+
+        mToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress);
+
+        setSupportActionBar(mToolbar);
+
+        mTextView = (TextView) findViewById(R.id.queryJSON);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mProgressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.search_action) {
+            Bundle bundle = new Bundle();
+            LoaderManager loaderManager = getSupportLoaderManager();
+            Loader<String> newsSearchLoader = loaderManager.getLoader(LOADER_ID);
+            if(newsSearchLoader == null){
+                loaderManager.initLoader(LOADER_ID, bundle, this).forceLoad();
+            }else{
+                loaderManager.restartLoader(LOADER_ID, bundle, this).forceLoad();
+            }
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private URL makeSearchUrl() {
+        URL newsSearchURL = NetworkUtils.buildUrl();
+        return newsSearchURL;
+    }
+
+    private void populateWithNewsData(String data){
+        mTextView.setText(data);
+    }
+
+    @Override
+    public android.support.v4.content.Loader<String> onCreateLoader(int id, final Bundle args) {
+        Log.d(TAG, NetworkUtils.buildUrl() + "\n\n\n\n\n");
+        return new AsyncTaskLoader<String>(this) {
+            @Override
+            protected void onStartLoading() {
+                Log.d(TAG, "onStartLoading called");
+                super.onStartLoading();
+                if(args == null){
+                    Log.d(TAG, "bundle null");
+                    return;
+                }
+                mTextView.setText(R.string.waiting_for_search);
+                mProgressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public String loadInBackground() {
+                Log.d(TAG, "loadInBackground called");
+
+                try {
+                    Log.d(TAG, "begin network call");
+                    newsSearchResults = NetworkUtils.getResponseFromHttpUrl(NetworkUtils.buildUrl());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Log.d(TAG, newsSearchResults);
+                return newsSearchResults;
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(android.support.v4.content.Loader<String> loader, String data) {
+        Log.d("mycode", data);
+        mProgressBar.setVisibility(View.GONE);
+        populateWithNewsData(data);
+    }
+
+    @Override
+    public void onLoaderReset(android.support.v4.content.Loader<String> loader) {}
 }
--- app/src/main/java/com/example/abel/newsapiandroidapp/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/abel/newsapiandroidapp/utilities/NetworkUtils.java
@@ -1,9 +1,60 @@
 package com.example.abel.newsapiandroidapp.utilities;
 
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class NetworkUtils {
     final static String NEWS_BASE_URL = "https://newsapi.org/v1/articles";
-    final static String NEWS_QUERY_SOURCE="the-next-web";
-    final static String NEWS_QUERY_SORTBY = "latest";
-    final static String NEWS_QUERY_APIKEY = SecretKeys.NEWS_API_KEY;
+
+    final static String PARAM_SOURCE = "source";
+    final static String PARAM_QUERY_SOURCE="the-next-web";
+
+    final static String PARAM_SORTBY = "sortBy";
+    final static String PARAM_QUERY_SORTBY = "latest";
+
+    final static String PARAM_APIKEY = "apiKey";
+    final static String PARAM_QUERY_APIKEY = SecretKeys.NEWS_API_KEY;
+
+    public static URL buildUrl() {
+        Uri builtUri = Uri.parse(NEWS_BASE_URL).buildUpon()
+                .appendQueryParameter(PARAM_SOURCE, PARAM_QUERY_SOURCE)
+                .appendQueryParameter(PARAM_SORTBY, PARAM_QUERY_SORTBY)
+                .appendQueryParameter(PARAM_APIKEY, PARAM_QUERY_APIKEY)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
 
 }
